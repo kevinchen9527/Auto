21fb5afbb26f1c8b6e313ef1a29855e6028d0616
kevin_chen <100823443@qq.com>
2022-06-14 12:42:53 +0800

    pdf转word

---
+++ pdf2word/main.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import logging
+from configparser import ConfigParser
+from concurrent.futures import ProcessPoolExecutor
+
+from pdf2docx import Converter
+
+
+def pdf_to_word(pdf_file_path, word_file_path):
+    cv = Converter(pdf_file_path)
+    cv.convert(word_file_path)
+    cv.close()
+
+
+def main():
+    logging.getLogger().setLevel(logging.ERROR)
+
+    config_parser = ConfigParser()
+    config_parser.read(os.path.abspath(os.path.dirname(__file__)) + "/config.cfg")
+    config = config_parser["default"]
+
+    tasks = []
+    with ProcessPoolExecutor(max_workers=int(config["max_worker"])) as executor:
+        for file in os.listdir(os.path.abspath(os.path.dirname(__file__)) +  '/' + config["pdf_folder"]):
+            extension_name = os.path.splitext(file)[1]
+            if extension_name != ".pdf":
+                continue
+            file_name = os.path.splitext(file)[0]
+            pdf_file = os.path.abspath(os.path.dirname(__file__)) + '/' + config["pdf_folder"] + "/" + file
+            word_file = os.path.abspath(os.path.dirname(__file__)) + '/' + config["word_folder"] + "/" + file_name + ".docx"
+            result = executor.submit(pdf_to_word, pdf_file, word_file)
+            tasks.append(result)
+    while True:
+        exit_flag = True
+        for task in tasks:
+            if not task.done():
+                exit_flag = False
+        if exit_flag:
+            print("完成")
+            exit(0)
+
+
+if __name__ == "__main__":
+    main()
